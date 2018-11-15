c6b9f4fe45e10523f5cf538dc6840720eed64fb3
Jakub Zárybnický <jakub.zarybnicky@inuits.eu>
2018-11-15 22:22:26 +0100

    Add myriprequest

--- src/myriprequest.c
+++ src/myriprequest.c
@@ -2,8 +2,51 @@
  * ISA18: Tools for monitoring and sniffing RIP messages
  * FIT VUT Brno
  * Author: xzaryb00 - Jakub Zarybnický (xzaryb00)
+ *
+ * A program that multicasts a RIPng Request packet
  */
 
+#include "myriprequest.h"
+#define USAGE "Usage: %s -i INTERFACE"
+
 int main (int argc, char *argv[]) {
+  int opt;
+  char *interface = NULL;
+
+  while ((opt = getopt(argc, argv, "i:")) != -1) {
+    switch (opt) {
+    case 'i':
+      interface = optarg;
+      break;
+    default:
+      fprintf(stderr, USAGE, argv[0]);
+      exit(EXIT_FAILURE);
+    }
+  }
+  if (interface == NULL)
+    ERR_USAGE("Missing required option `-i INTERFACE`\n");
+
+  struct rip6hdr header = { .rip6_cmd = RIP_CMD_REQUEST, .rip6_ver = 1 };
+  struct rip6_entry entry = { .rip6_metric = 16 };
+
+  //Copy all three parts into the buffer
+  char buffer[512] = { 0 };
+  uint buflen = 0;
+  memcpy(buffer, &header, sizeof(header));
+  buflen += sizeof(header);
+  memcpy(buffer + buflen, &entry, sizeof(entry));
+  buflen += sizeof(entry);
+
+  struct sockaddr_in6 target = { .sin6_family = AF_INET6,
+                                 .sin6_port = htons(RIPNG_PORT),
+                                 .sin6_flowinfo = 0 };
+  inet_pton(AF_INET6, RIPNG_DEST, &target.sin6_addr);
+
+  int sock = prepare_socket(interface);
+
+  printf("Sending message... ");
+  SOCK_WRAP(sock, "sendto()",
+            sendto(sock, buffer, buflen, 0, (struct sockaddr *) &target, sizeof(target)) < 0);
+  printf("OK\n");
   return 0;
 }
---
+++ src/myriprequest.h
@@ -0,0 +1,18 @@
+/**
+ * ISA18: Tools for monitoring and sniffing RIP messages
+ * FIT VUT Brno
+ * Author: xzaryb00 - Jakub Zarybnický (xzaryb00)
+ *
+ * A program that multicasts a RIPng Request packet
+ */
+
+#ifndef ISA_MYRIPRESPONSE_H
+#define ISA_MYRIPRESPONSE_H
+
+#include <arpa/inet.h>
+#include <stdio.h>
+#include "socket.h"
+
+int main(int argc, char *argv[]);
+
+#endif /* ISA_MYRIPRESPONSE_H */
